a6ab815f38beae944080c9647de32fa384fee9ad
Shannon Dillon <sedillon93@gmail.com>
2018-10-24 21:01:35 -0700

    add pictures for animal dropdown

--- src/gettingStarted.js
+++ src/gettingStarted.js
@@ -14,19 +14,38 @@ class GetStarted extends React.Component{
     this.setState({
       animal: animal
     });
+    let image = document.getElementById("animalImage");
+    switch (animal){
+      case 'bear':
+        image.src = 'src/images/panda.png';
+        break;
+      case 'bunny':
+        image.src = 'src/images/bunny.png';
+        break;
+      case 'cat':
+        image.src = 'src/images/cat.png';
+        break;
+      case 'owl':
+        image.src = 'src/images/owl.png';
+        break;
+      default:
+        image.src = '';
+        break;
+    }
   }
 
   render() {
     return (
       <div>
-        <label for="animalSelect">What animal would you like to build today?</label>
-        <select id="animalSelect" required="true" onChange={() => this.setAnimal(document.getElementById("animalSelect").value)}>
+        <label for="animalSelection">What animal would you like to build today?</label>
+        <select id="animalSelection" required="true" onChange={() => this.setAnimal(document.getElementById("animalSelection").value)}>
           <option value="">Choose an animal</option>
           <option value="bear">bear</option>
           <option value="bunny">bunny</option>
           <option value="cat">cat</option>
           <option value="owl">owl</option>
         </select>
+        <img id="animalImage" src=""/>
       </div>
     )
   }
